91bcc80a7712ac2e63f9b5eb122630f3ea1fea47
Joel Buentello <joel.buentello@yahoo.com>
2019-10-24 10:30:44 -0500

    Fixed folder structure, APP contained data, public, and routing

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 //Set up dependencies
 var express = require("express");
 var path = require("path");
-var bodyparser = require("body-parser");
+var bodyParser = require("body-parser");
 
 //Express configuration
 var app = express();
 var PORT = process.env.PORT;
 
+//Directory for CSS files
+app.use(express.static(__dirname + "/app/css"));
 
+//Middleware for parsing JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
+//Routes for API and HTML
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
 
