77b7427dc7db19e0b4c39210a78b445159f85454
Joel Buentello <joel.buentello@yahoo.com>
2019-10-24 10:12:16 -0500

    Added Dependencies Express, bodyParser, path

---
+++ server.js
@@ -0,0 +1,12 @@
+//Set up dependencies
+var express = require("express");
+var path = require("path");
+var bodyparser = require("body-parser");
+
+//Express configuration
+var app = express();
+var PORT = process.env.PORT;
+
+
+
+
